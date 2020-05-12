90699903c21d1e5752a4bd04d626de295736e373
LaurentNevou <35040499+LaurentNevou@users.noreply.github.com>
2020-05-12 11:07:57 +0200

    Update Transmission_DFB_f.m

--- Transmission_DFB_f.m
+++ Transmission_DFB_f.m
@@ -1,5 +1,12 @@
 function[T,R]=Transmission_DFB_f(lambda,Gain,L,LL,n1,n2,Dn2,n3)
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% "Optoelectronic", Cambridge Books Online% Prof. Emmanuel Rosencher,
+% Complement to Chapter 9
+% 9.D Fabry–Perot cavities and Bragg reflectors, page 434
+% http://dx.doi.org/10.1017/CBO9780511754647.028
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 c=2.99792458e8;             %% speed of light [m/s]
 k=2*pi/lambda;              %% wave vector    [m-1]
 w=2*pi*c/lambda;            %% pulsation      [s-1]
